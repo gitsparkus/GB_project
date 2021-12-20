f8863abdadbba13e012732c32027739549752111
Aleksandr <aleksandrov.a.a@gmail.com>
2021-12-20 14:21:04 +0200

    second

--- task-2,3.py
+++ task-2,3.py
@@ -1,14 +1,16 @@
 lst = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+05', 'градусов']
 
-lst1 = []
-
-for item in lst:
+i = 0
+while i < len(lst):
     sign = ''
+    item = lst[i]
     if item[0] in ('+', '-') and item[1:].isnumeric():
         sign, item = item[0], item[1:]
     if item.isnumeric():
-        lst1 += ['"'] + [sign + item.zfill(2)] + ['"']
-    else:
-        lst1 += [item]
+        lst[i] = lst[i].zfill(2)
+        lst.insert(i, '"')
+        lst.insert(i + 2, '"')
+        i += 2
+    i += 1
 
-print(''.join(lst1))
+print(lst)
