408143eb2dde0b1f1d95115d5fe40399b543af63
Aleksandr <aleksandrov.a.a@gmail.com>
2022-01-17 16:11:52 +0200

    done

--- lesson-7/task-3/task-3.py
+++ lesson-7/task-3/task-3.py
@@ -9,24 +9,17 @@ def make_folder(full_path):
     try:
         os.mkdir(full_path)
     except FileExistsError as e:
-        print(f'Каталог "{full_path}" уже существует.')
+        pass  # print(f'Каталог "{full_path}" уже существует.')
 
-
-dict_of_path = {}
-for (dirpath, dirnames, filenames) in os.walk(ROOT):
-    for filename in filenames:
-        if filename.endswith('.html'):
-            if dirpath in dict_of_path:
-                dict_of_path[dirpath].append(filename)
-            else:
-                dict_of_path[dirpath] = [filename]
 
 make_folder(TEMPLATE_PATH)
-for path, files in dict_of_path.items():
-    new_path = os.path.join(TEMPLATE_PATH, os.path.split(path)[1])
-    make_folder(new_path)
-    for file in files:
-        try:
-            shutil.copy(os.path.join(path, file), os.path.join(new_path, file))
-        except shutil.SameFileError as e:
-            print(f'Файл "{os.path.join(new_path, file)}" уже существует.')
+
+for (dir_path, dir_names, filenames) in os.walk(ROOT):
+    for filename in filenames:
+        if filename.endswith('.html'):
+            new_path = os.path.join(TEMPLATE_PATH, os.path.split(dir_path)[1])
+            make_folder(new_path)
+            try:
+                shutil.copy(os.path.join(dir_path, filename), os.path.join(new_path, filename))
+            except shutil.SameFileError as e:
+                pass  # print(f'Файл "{os.path.join(new_path, filename)}" уже существует.')
